9c30e58d14ada518a252ca6247c45bd7b967bb7a
孙贺 <sunhe@deepdata.com.cn>
2016-05-09 18:34:36 +0800

    add DataWrapper

---
+++ src/main/java/cn/com/deepdata/es_adapter/model/DataWrapper.java
@@ -0,0 +1,39 @@
+package cn.com.deepdata.es_adapter.model;
+
+/**
+ * Thread-safe.
+ * 
+ * @author sunhe
+ * @date 2016年5月9日
+ */
+public class DataWrapper {
+	
+	private Object data;
+	
+	/**
+	 * first processing adapter's class literal
+	 */
+	private Class<?> firstAdapterClazz;
+	
+	public DataWrapper(Object data) {
+		this(data, null);
+	}
+	
+	public DataWrapper(Object data, Class<?> firstAdapterClazz) {
+		this.data = data;
+		this.firstAdapterClazz = firstAdapterClazz;
+	}
+
+	public synchronized Object getData() {
+		return data;
+	}
+
+	public synchronized Class<?> getFirstAdapterClazz() {
+		return firstAdapterClazz;
+	}
+
+	public synchronized void setFirstAdapterClazz(Class<?> firstAdapterClazz) {
+		this.firstAdapterClazz = firstAdapterClazz;
+	}
+	
+}
