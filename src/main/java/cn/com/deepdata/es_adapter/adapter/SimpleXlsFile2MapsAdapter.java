65f3db566d8f68ee675555e754becf33661cbb58
孙贺 <sunhe@deepdata.com.cn>
2016-06-23 14:16:13 +0800

    finish xls adapter

--- src/main/java/cn/com/deepdata/es_adapter/adapter/SimpleXlsFile2MapsAdapter.java
+++ src/main/java/cn/com/deepdata/es_adapter/adapter/SimpleXlsFile2MapsAdapter.java
@@ -2,81 +2,237 @@ package cn.com.deepdata.es_adapter.adapter;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row.MissingCellPolicy;
 
 import cn.com.deepdata.es_adapter.SkipAdaptingException;
 
-public class XlsFile2MapsAdapter extends AbstractAdapter implements QueueDataProvidingAdapter {
+/**
+ * A simple adapter that adapts Microsoft Excel 97/2000/XP/2003 work sheet file (.xls) 
+ * to a list of {@link Map}s (every adapted map will be populated to the {@link AdapterChain} 
+ * individually, so immediately subsequent adapter of this one should be capable of 
+ * processing {@link Map}, not a list of maps).
+ * <p/>
+ * This adapter is simplified, not very versatile - only supporting 1-layer, 2-dimensional 
+ * layout of data. More specifically, the data to adapt should be like a 2-dimensional table, 
+ * without inner complicated enclosing structure in it. And you can extend this class to 
+ * provide more functionalities.
+ * <p/>
+ * This adapter can accept either {@link File} or {@link FileInputStream} representing the work 
+ * sheet file to adapt as input.
+ */
+public class SimpleXlsFile2MapsAdapter extends AbstractAdapter implements QueueDataProvidingAdapter {
 	
 	protected final List<String> titleList;
 	
 	protected final int startRowNum;
-	
 	protected final int endRowNum;
 	
-	protected final List<Integer> sheetIndeciesToAdapt;
-	
+	protected final List<Integer> sheetIndicesToAdapt;
 	protected final List<String> sheetNamesToAdapt;
 	
-	public XlsFile2MapsAdapter(List<String> titleList) {
+	/**
+	 * @param titleList
+	 * 		titles applied to each cell of a row
+	 */
+	public SimpleXlsFile2MapsAdapter(List<String> titleList) {
 		this(titleList, 0, Integer.MAX_VALUE);
 	}
 	
-	public XlsFile2MapsAdapter(List<String> titleList, int startRowNum) {
+	/**
+	 * @param titleList
+	 * 		titles applied to each cell of a row
+	 * @param startRowNum
+	 * 		number of row, from which and above to adapt. 
+	 * 		0-based, inclusive. 
+	 */
+	public SimpleXlsFile2MapsAdapter(List<String> titleList, int startRowNum) {
 		this(titleList, startRowNum, Integer.MAX_VALUE);
 	}
 	
-	public XlsFile2MapsAdapter(List<String> titleList, int startRowNum, int endRowNum) {
+	/**
+	 * @param titleList
+	 * 		titles applied to each cell of a row
+	 * @param startRowNum
+	 * 		number of row, from which and above to adapt. 
+	 * 		0-based, inclusive. 
+	 * @param endRowNum
+	 * 		number of row, the last row to adapt. 
+	 * 		0-based, exclusive.
+	 */
+	public SimpleXlsFile2MapsAdapter(List<String> titleList, int startRowNum, int endRowNum) {
 		this(titleList, null, null, startRowNum, endRowNum);
 	}
 	
-	public XlsFile2MapsAdapter(List<String> titleList, List<Integer> sheetIndeciesToAdapt) {
-		this(titleList, sheetIndeciesToAdapt, 0);
+	/**
+	 * @param titleList
+	 * 		titles applied to each cell of a row
+	 * @param sheetIndicesToAdapt
+	 * 		index of sheets to adapt
+	 */
+	public SimpleXlsFile2MapsAdapter(List<String> titleList, List<Integer> sheetIndicesToAdapt) {
+		this(titleList, sheetIndicesToAdapt, 0);
 	}
 	
-	public XlsFile2MapsAdapter(List<String> titleList, List<Integer> sheetIndeciesToAdapt, int startRowNum) {
-		this(titleList, sheetIndeciesToAdapt, null, startRowNum, Integer.MAX_VALUE);
+	/**
+	 * @param titleList
+	 * 		titles applied to each cell of a row
+	 * @param sheetIndicesToAdapt
+	 * 		index of sheets to adapt
+	 * @param startRowNum
+	 * 		number of row, from which and above to adapt. 
+	 * 		0-based, inclusive. 
+	 */
+	public SimpleXlsFile2MapsAdapter(List<String> titleList, List<Integer> sheetIndicesToAdapt, int startRowNum) {
+		this(titleList, sheetIndicesToAdapt, null, startRowNum, Integer.MAX_VALUE);
 	}
 	
-	public XlsFile2MapsAdapter(List<String> titleList, int startRowNum, List<String> sheetNamesToAdapt) {
+	/**
+	 * @param titleList
+	 * 		titles applied to each cell of a row
+	 * @param startRowNum
+	 * 		number of row, from which and above to adapt. 
+	 * 		0-based, inclusive. 
+	 * @param sheetNamesToAdapt
+	 * 		name of sheets to adapt
+	 */
+	public SimpleXlsFile2MapsAdapter(List<String> titleList, int startRowNum, List<String> sheetNamesToAdapt) {
 		this(titleList, null, sheetNamesToAdapt, startRowNum, Integer.MAX_VALUE);
 	}
 	
-	private XlsFile2MapsAdapter(List<String> titleList, List<Integer> sheetIndeciesToAdapt, List<String> sheetNamesToAdapt, 
-			int startRowNum, int endRowNum) {
+	/**
+	 * At least one of the parameters of 'sheetIndicesToAdapt' and 'sheetNamesToAdapt' 
+	 * should be null, or only 'sheetIndicesToAdapt' is applied.
+	 * 
+	 * @param titleList
+	 * 		titles applied to each cell of a row
+	 * @param sheetIndicesToAdapt
+	 * 		index of sheets to adapt
+	 * @param sheetNamesToAdapt
+	 * 		name of sheets to adapt
+	 * @param startRowNum
+	 * 		number of row, from which and above to adapt. 
+	 * 		0-based, inclusive. 
+	 * @param endRowNum
+	 * 		number of row, the last row to adapt. 
+	 * 		0-based, exclusive.
+	 */
+	protected SimpleXlsFile2MapsAdapter(List<String> titleList, List<Integer> sheetIndicesToAdapt, 
+			List<String> sheetNamesToAdapt, int startRowNum, int endRowNum) {
 		this.titleList = titleList;
-		this.sheetIndeciesToAdapt = Collections.unmodifiableList(sheetIndeciesToAdapt);
-		this.sheetNamesToAdapt = Collections.unmodifiableList(sheetNamesToAdapt);
+		this.sheetIndicesToAdapt = sheetIndicesToAdapt == null ? 
+				null : Collections.unmodifiableList(sheetIndicesToAdapt);
+		this.sheetNamesToAdapt = sheetNamesToAdapt == null ? 
+				null : Collections.unmodifiableList(sheetNamesToAdapt);
 		this.startRowNum = startRowNum;
 		this.endRowNum = endRowNum;
 	}
 	
-	protected void adaptRow(HSSFRow row) {
-		// TODO
+	/*
+	 * You can override the following extract* methods to 
+	 * change the default behavior how to extract value from 
+	 * different types of cells.
+	 */
+	protected Object extractValueFromStringTypeCell(HSSFCell cell) {
+		return cell.getStringCellValue();
+	}
+	protected Object extractValueFromNumericTypeCell(HSSFCell cell) {
+		return cell.getNumericCellValue();
+	}
+	protected Object extractValueFromBooleanTypeCell(HSSFCell cell) {
+		return cell.getBooleanCellValue();
+	}
+	protected Object extractValueFromBlankCell(HSSFCell cell) {
+		return null;
+	}
+	protected Object extractValueFromOtherTypeCell(HSSFCell cell) {
+		throw new IllegalStateException("cell type not supported");
 	}
 	
-	protected void adaptSheet(HSSFSheet sheet) {
-		// TODO
+	protected void adaptRow(HSSFRow row, AdapterContext ctx) throws InterruptedException {
+		Map<String, Object> dataMap = new HashMap<String, Object>();
+		
+		for (int c = 0; c < titleList.size(); c++) {
+			HSSFCell cell = row.getCell(c, MissingCellPolicy.CREATE_NULL_AS_BLANK);
+			String title = titleList.get(c);
+			
+			int cellType = cell.getCellType();
+			if (cellType == HSSFCell.CELL_TYPE_STRING) {
+				dataMap.put(title, extractValueFromStringTypeCell(cell));
+			}
+			else if (cellType == HSSFCell.CELL_TYPE_NUMERIC) {
+				dataMap.put(title, extractValueFromNumericTypeCell(cell));
+			}
+			else if (cellType == HSSFCell.CELL_TYPE_BOOLEAN) {
+				dataMap.put(title, extractValueFromBooleanTypeCell(cell));
+			}
+			else if (cellType == HSSFCell.CELL_TYPE_BLANK) {
+				dataMap.put(title, extractValueFromBlankCell(cell));
+			}
+			else {
+				dataMap.put(title, extractValueFromOtherTypeCell(cell));
+			}
+		}
+		
+		putData(dataMap, ctx.getNextAdapterClazz());
+	}
+	
+	protected void adaptSheet(HSSFSheet sheet, AdapterContext ctx) throws InterruptedException {
+		for (int r = startRowNum; r < sheet.getLastRowNum() + 1 && r < endRowNum; r++) {
+			HSSFRow row = sheet.getRow(r);
+			if (row != null) {
+				adaptRow(row, ctx);
+			}
+		}
 	}
 	
 	@Override
 	public Object inboundAdapt(Object data, AdapterContext ctx) throws Exception {
 		FileInputStream in = null;
+		HSSFWorkbook workbook = null;
 		
 		try {
 			in = data instanceof File ? new FileInputStream((File) data) : (FileInputStream) data;
-			HSSFWorkbook workbook = new HSSFWorkbook(in);
+			workbook = new HSSFWorkbook(in);
+			
+			List<HSSFSheet> sheetsToAdapt = new ArrayList<HSSFSheet>();
+			if (sheetIndicesToAdapt != null) {
+				for (int sheetIndex : sheetIndicesToAdapt) {
+					sheetsToAdapt.add(workbook.getSheetAt(sheetIndex));
+				}
+			}
+			else if (sheetNamesToAdapt != null) {
+				for (String sheetName : sheetNamesToAdapt) {
+					sheetsToAdapt.add(workbook.getSheet(sheetName));
+				}
+			}
+			else  {
+				for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+					sheetsToAdapt.add(workbook.getSheetAt(i));
+				}
+			}
+			
+			for (HSSFSheet sheet : sheetsToAdapt) {
+				adaptSheet(sheet, ctx);
+			}
 			
 			throw new SkipAdaptingException();
 		}
 		finally {
 			if (in != null) {
 				in.close();
+			}
+			if (workbook != null) {
+				workbook.close();
 			}
 		}
 	}
--- src/test/java/cn/com/deepdata/es_adapter/ClientTest.java
+++ src/test/java/cn/com/deepdata/es_adapter/ClientTest.java
@@ -1,27 +1,63 @@
 package cn.com.deepdata.es_adapter;
 
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.ImmutableSettings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import cn.com.deepdata.es_adapter.Pipeline.PipelineSettings;
+import cn.com.deepdata.es_adapter.adapter.AbstractAdapter;
+import cn.com.deepdata.es_adapter.adapter.AdapterChain;
+import cn.com.deepdata.es_adapter.adapter.AdapterChainInitializer;
+import cn.com.deepdata.es_adapter.adapter.AdapterContext;
+import cn.com.deepdata.es_adapter.adapter.SimpleXlsFile2MapsAdapter;
+
 public class ClientTest {
 
-	@SuppressWarnings("resource")
 	@Ignore
 	@Test
-	public void test() {
-		Client client = new TransportClient(ImmutableSettings.settingsBuilder()
-				.put("cluster.name", "elasticsearch")
-				.put("client.transport.sniff", true)
-				.build())
-		.addTransportAddress(new InetSocketTransportAddress("221.122.121.96", 19300));
-		
-		client.prepareIndex("copyright", "software_copyright").setSource("{\"message\":\"hello, world\"}").execute().actionGet();
-		
-		client.close();
+	public void test() throws InterruptedException {
+		PipelineSettings settings = PipelineSettings.builder()
+				.index("xls-index")
+				.type("xls-type")
+				.timeoutAfterClosing(5)
+				.threadPoolSize(1)
+				.build();
+		Pipeline pipeline = Pipeline.build(settings, new AdapterChainInitializer() {
+			
+			@Override
+			public void initialize(AdapterChain adapterChain) {
+				List<String> titleList = new ArrayList<String>();
+				titleList.add("a");
+				titleList.add("b");
+				titleList.add("c");
+				titleList.add("d");
+				adapterChain.addLast(new SimpleXlsFile2MapsAdapter(titleList, 1, 3));
+				adapterChain.addLast(new AbstractAdapter() {
+					
+					private final ObjectMapper objectMapper = new ObjectMapper();
+					
+					@Override
+					public Object outboundAdapt(Object data, AdapterContext ctx) throws Exception {
+						return data;
+					}
+					
+					@Override
+					public Object inboundAdapt(Object data, AdapterContext ctx) throws Exception {
+						System.out.println(objectMapper.writeValueAsString(data));
+						return data;
+					}
+				
+				});
+			}
+			
+		});
+		pipeline.putData(new File("C:\\Users\\deepdata\\Desktop\\test.xls"));
+		pipeline.close();
 	}
 
 }
