24a1b38b5819c8c21a00c6d20569ae5aa142565e
bedew <sunhe1007@126.com>
2017-01-08 22:23:24 -0500

    bug fix when using dir util

--- src/main/java/cn/com/deepdata/es_adapter/util/DirectoryAdaptUtil.java
+++ src/main/java/cn/com/deepdata/es_adapter/util/DirectoryAdaptUtil.java
@@ -11,45 +11,45 @@ import cn.com.deepdata.es_adapter.Pipeline;
 import cn.com.deepdata.es_adapter.PipelineFactory;
 
 /**
- * Utility that helps you adapt multiple files in a 
+ * Utility that helps you adapt multiple files in a
  * same directory.
  * <p/>
- * You assign the root directory, and the file name 
- * suffix (typically extension name), then this utility 
- * class will find all of the file with the suffix name 
+ * You assign the root directory, and the file name
+ * suffix (typically extension name), then this utility
+ * class will find all of the file with the suffix name
  * assigned by you, and adapt them.
  */
 public class DirectoryAdaptUtil {
 
 	protected PipelineFactory pipelineFactory;
-	
+
 	protected String suffix;
-	
+
 	protected List<File> xlsFileList = new ArrayList<File>();
-	
+
 	public DirectoryAdaptUtil(PipelineFactory pipelineFactory, String rootDir, String suffix) {
 		this.pipelineFactory = pipelineFactory;
 		this.suffix = suffix;
 		findXlsFiles(new File(rootDir));
 	}
-	
+
 	protected void findXlsFiles(File dir) {
 		File[] xlsFiles = dir.listFiles(new FilenameFilter() {
-			
+
 			public boolean accept(File dir, String name) {
-				if (name.endsWith("suffix")) {
+				if (name.endsWith(suffix)) {
 					return true;
 				}
 				else {
 					return false;
 				}
 			}
-			
+
 		});
 		xlsFileList.addAll(new ArrayList<File>(Arrays.asList(xlsFiles)));
-		
+
 		File[] innerDirs = dir.listFiles(new FileFilter() {
-			
+
 			public boolean accept(File pathname) {
 				if (pathname.isDirectory()) {
 					return true;
@@ -58,13 +58,13 @@ public class DirectoryAdaptUtil {
 					return false;
 				}
 			}
-			
+
 		});
 		for (File innerDir : innerDirs) {
 			findXlsFiles(innerDir);
 		}
 	}
-	
+
 	protected void transASingleXlsFile(File file) throws InterruptedException {
 		System.out.println("transfering file: " + file.getAbsolutePath());
 		Pipeline pipeline = pipelineFactory.getInstance();
@@ -72,11 +72,11 @@ public class DirectoryAdaptUtil {
 		pipeline.close();
 		System.out.println("done: " + file.getAbsolutePath());
 	}
-	
+
 	public void start() throws InterruptedException {
 		for (File xlsFile : xlsFileList) {
 			transASingleXlsFile(xlsFile);
 		}
 	}
-	
+
 }
